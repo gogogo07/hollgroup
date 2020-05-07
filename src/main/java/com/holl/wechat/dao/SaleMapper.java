218ec684430c9e40930977d08f6c533d3a5fdfef
hcmii <43982971+hcmii@users.noreply.github.com>
2020-05-07 22:01:38 +0800

    2020-5-7 二手交易接口的完成

---
+++ src/main/java/com/holl/wechat/dao/SaleMapper.java
@@ -0,0 +1,22 @@
+package com.holl.wechat.dao;
+
+import com.holl.wechat.model.Sale;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SaleMapper {
+
+    List<Sale> findAll();
+
+    void publishSale(Sale sale);
+
+    void updateSale(Sale sale);
+
+    void cancelSale(long id);
+
+    List<Sale> findSaleByOpenId(String openId);
+
+}
