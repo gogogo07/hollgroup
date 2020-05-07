a5a0e2b33022b020411d83f7d020de8b340755cc
hcmii <43982971+hcmii@users.noreply.github.com>
2020-05-07 22:06:02 +0800

    2020-5-7 二手交易接口完成

---
+++ src/main/java/com/holl/wechat/service/SaleService.java
@@ -0,0 +1,19 @@
+package com.holl.wechat.service;
+
+import com.holl.wechat.model.Sale;
+
+import java.util.List;
+
+public interface SaleService {
+
+    List<Sale> findAll();
+
+    void publishSale(Sale sale);
+
+    void updateSale(Sale sale);
+
+    void cancelSale(long id);
+
+    List<Sale> findSaleByOpenId(String openId);
+
+}
